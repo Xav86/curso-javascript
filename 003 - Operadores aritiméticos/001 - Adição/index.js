40bf77c34fa3c14d41678fef437448dddb4667d4
Xav86 <gustavo.g.2005@gmail.com>
2024-06-12 17:39:32 -0300

    visto operadores aritiméticos

---
+++ 003 - Operadores aritiméticos/001 - Adição/index.js
@@ -0,0 +1,6 @@
+console.log(1 + 1);
+console.log(25 + 50);
+
+console.log("Gustavo " + "Gonçalves");
+console.log("Nome: " + "Gustavo Gonçalves" + ", " + "idade: " + 18 + 1);
+console.log("Nome: " + "Gustavo Gonçalves" + ", " + "idade: " + (18 + 1));
---
+++ 003 - Operadores aritiméticos/006 - Comparação/index.js
@@ -0,0 +1,16 @@
+console.log(1 == 1);
+console.log(1 == "1");
+console.log(1 === 1);
+
+console.log(1 != "1");
+console.log(1 !== 1);
+
+console.log(3 > 2);
+console.log(3 < 5);
+
+console.log(2 >= 3);
+console.log(1 <= 1);
+
+/*
+    ==, ===, !=, !==, >, <, >=, <=
+*/
