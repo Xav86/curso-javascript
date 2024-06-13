b7c1218ac76b9988301c9051ea4abd6aecbe04f3
Xav86 <gustavo.g.2005@gmail.com>
2024-06-13 08:47:22 -0300

    Visto condicionais

---
+++ 005 - Condicionais/001 - if - else/index.js
@@ -0,0 +1,17 @@
+/*
+let soma = -10;
+
+if ( soma >= 0 ) {
+    console.log("Você está com saldo positivo");
+} else {
+    console.log("Você está com o saldo negativo");
+}
+*/
+
+if ( 1 === 0 ) {
+    console.log("opção 1");
+} else if (1 === 1) {
+    console.log("opção 2");
+} else {
+    console.log("opção 3");
+}
---
+++ 005 - Condicionais/003 - Operador Ternário/index.js
@@ -0,0 +1,14 @@
+const velocidade = 86;
+const warn = 85;
+
+// condição ? true : false;
+
+const condicao = velocidade >= warn 
+? console.log("Multado")
+: console.log("bom dia");
+
+/*
+if ( velocidade >= warn ) {
+    console.log("Multado")
+}
+*/
