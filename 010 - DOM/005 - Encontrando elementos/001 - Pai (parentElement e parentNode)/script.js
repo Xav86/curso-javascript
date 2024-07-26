03bd67ddd8574f071314fe1d61d409acf17f2d89
Xav86 <gustavo.g.2005@gmail.com>
2024-07-26 15:05:59 -0300

    vendo como pegar um elemento pai

---
+++ 010 - DOM/005 - Encontrando elementos/001 - Pai (parentElement e parentNode)/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const strong = document.querySelector('strong');
+const p = document.querySelector('.paragrafo');
+const container = document.querySelector('.container');
+const body = document.querySelector('body');
+
+console.log(p.parentElement);
+console.log(p.parentNode);
+console.log(strong.parentElement);
+console.log(strong.parentNode);
+console.log(container.parentElement);
+console.log(body.parentNode);
