a9818a0c3c6bf2e639af1d24774b283a2005d632
Xav86 <gustavo.g.2005@gmail.com>
2024-07-29 11:17:02 -0300

    visto como pegar elementos filhos

---
+++ 010 - DOM/005 - Encontrando elementos/003 - Filhos (firstChild, firstElementChild)/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const container = document.querySelector('.container');
+const p = document.querySelector('.paragrafo');
+
+console.log(p.firstChild);
+console.log(p.firstElementChild);
+
+console.log(container.firstChild);
+console.log(container.firstElementChild);
