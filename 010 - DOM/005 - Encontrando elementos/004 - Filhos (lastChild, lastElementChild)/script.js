c27460d0099e4b8cfcc6f3103fd875c4d1ee5f53
Xav86 <gustavo.g.2005@gmail.com>
2024-07-30 09:14:02 -0300

    visto como pegar ultimo elemento filho

---
+++ 010 - DOM/005 - Encontrando elementos/004 - Filhos (lastChild, lastElementChild)/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const container = document.querySelector('.container');
+const p = document.querySelector('.paragrafo');
+
+console.log(p.lastChild);
+console.log(p.lastElementChild);
+
+console.log(container.lastChild);
+console.log(container.lastElementChild);
